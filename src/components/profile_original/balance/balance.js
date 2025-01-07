321cc327256966b4b97222136279b49dc9ec5664
hernan066 <hernan0066@gmail.com>
2025-01-07 13:22:41 -0300

    Update balance

--- src/components/profile_original/balance/balance.js
+++ src/components/profile_original/balance/balance.js
@@ -1,38 +1,48 @@
-import { formatUnits } from "viem";
+"use client";
+import { useSelector } from "react-redux";
 import styles from "./balance.module.css";
+import { useGetBalance } from "@/hooks/useGetBalance";
+import { useAccount } from "wagmi";
 
-const Balance_original = ({ data, priceTrcData, priceBnbData }) => {
-  const balanceTrc = +formatUnits(data?.balanceTrc || "", 18);
+const Balance_original = () => {
+  useGetBalance();
+  const { address } = useAccount();
 
-  const balanceBnb = +data.balanceBnb;
-  const balanceBnbFix = Number.isInteger(balanceBnb)
-    ? balanceBnb.toString()
-    : balanceBnb.toFixed(6);
+  const { TRCPrice, BNBPrice, amountTRC, amountBNB, isLoading, errorBalance } =
+    useSelector((state) => state.balance);
 
-  const balanceTrcFix = Number.isInteger(balanceTrc)
-    ? balanceTrc.toString()
-    : balanceTrc.toFixed(6);
-
-  const trcUsd = priceTrcData?.pairs[0]?.priceUsd * balanceTrc;
-  const bnbUsd = priceBnbData?.pairs[0]?.priceUsd * data.balanceBnb;
+  const trcUsd = (TRCPrice * amountTRC).toFixed(2);
+  const bnbUsd = (BNBPrice * amountBNB).toFixed(2);
 
   return (
-    <div className={styles.container}>
-      <p className={styles.label}>Wallet </p>
-      <div className={styles.row}>
-        <p className={styles.data}>{data.address}</p>
+    <>
+      {isLoading && (
+        <div className={styles.loading}>
+          <span className="loader"></span>
+        </div>
+      )}
+      {errorBalance && (
+        <div className={styles.error} style={{ color: "red" }}>
+          <p>{errorBalance}</p>
+        </div>
+      )}
+      <div className={styles.container}>
+        <p className={styles.label}>Wallet </p>
+        <div className={styles.row}>
+          <p className={styles.data}>{address}</p>
+        </div>
+        <p className={styles.label}>TRC </p>
+        <div className={styles.row}>
+          <p className={styles.data2}>{amountTRC}</p>
+          <p className={styles.data3}>{`$${trcUsd || 0}`}</p>
+        </div>
+        <p className={styles.label}>BNB </p>
+        <div className={styles.row}>
+          <p className={styles.data2}>{amountBNB}</p>
+          <p className={styles.data3}>{`$${bnbUsd || "0"}`}</p>
+        </div>
       </div>
-      <p className={styles.label}>TRC </p>
-      <div className={styles.row}>
-        <p className={styles.data2}>{balanceTrcFix}</p>
-        <p className={styles.data3}>{`$${trcUsd.toFixed(2)}`}</p>
-      </div>
-      <p className={styles.label}>BNB </p>
-      <div className={styles.row}>
-        <p className={styles.data2}>{balanceBnbFix}</p>
-        <p className={styles.data3}>{`$${bnbUsd.toFixed(2)}`}</p>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/components/profile_original/main/Main.js
+++ src/components/profile_original/main/Main.js
@@ -1,50 +1,18 @@
 "use client";
 
-import { useAccount, useBalance, useReadContract } from "wagmi";
-import { ABI_TRC, TRC_CONTRACT } from "../../../../data/data_exchange";
-import { useFetch } from "@/hooks/useFetch";
+import { useAccount } from "wagmi";
+
 import styles from "./main.module.css";
 import Balance_original from "../balance/balance";
 
 const MainProfile = ({ translations }) => {
   const { connect } = translations;
   const { address } = useAccount();
-  const { data: priceTrcData, loading: l1 } = useFetch(
-    "0x34B08ccf9620aEd6d158BaE65e85Bb3bBe2c384A"
-  );
-  const { data: priceBnbData, loading: l2 } = useFetch(
-    "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
-  );
-
-  const { data: balanceTrc, isLoading: l3 } = useReadContract({
-    abi: ABI_TRC,
-    address: TRC_CONTRACT,
-    functionName: "balanceOf",
-    args: [address], //wallet, address
-  });
-
-  const { data: balanceBnb, isLoading: l4 } = useBalance({
-    address,
-  });
 
   return (
     <section className={styles.container}>
       <w3m-button balance="false" size="sm" label={connect} />
-      {l1 && l2 && l3 && l4 && (
-        <div className={styles.loading}>
-          <span className="loader"></span>
-        </div>
-      )}
-
-      <Balance_original
-        data={{
-          address,
-          balanceTrc,
-          balanceBnb: balanceBnb?.formatted,
-        }}
-        priceTrcData={priceTrcData}
-        priceBnbData={priceBnbData}
-      />
+      {address && <Balance_original />}
     </section>
   );
 };
